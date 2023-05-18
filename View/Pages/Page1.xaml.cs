be2ced9cf1e4ffdda3f800f6b7664a8f1b1c4615
KNGCoder <kudnad2016@yandex.ru>
2023-05-18 09:28:42 +0300

    nhgvc

--- View/Pages/Page1.xaml.cs
+++ View/Pages/Page1.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Task6._1.Core;
+using Task6._1.Model;
 
 namespace Task6._1.View.Pages
 {
@@ -20,19 +22,42 @@ namespace Task6._1.View.Pages
     /// </summary>
     public partial class Page1 : Page
     {
+        private ModelContext? _db = null;
         public Page1()
         {
             InitializeComponent();
+            _db = new ModelContext();
         }
 
         private void BtnLogin_Click(object sender, RoutedEventArgs e)
         {
+            User? userModel = _db?.Users.FirstOrDefault(predicate: x => x.Login == TbLogin.Text && x.Password == PbPassword.Text);
+            if (userModel != null) 
+            {
+                switch (userModel.Role.RoleID) 
+                {
+                    case 1:
+                        MessageBox.Show("Администратор", "Системное сообщение",MessageBoxButton.OK, MessageBoxImage.Information);
+                        break;
 
+                    case 2:
+                        MessageBox.Show("Пользователь", "Системное сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                        break;
+                    
+                    case 3:
+                        MessageBox.Show("Гость ", "Системное сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка ввода данных", "Системное сообщение", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void BtnRegistration_Click(object sender, RoutedEventArgs e)
         {
-
+            Util.UtilFrame?.Navigate(new RegistrationPage());
         }
     }
 }
---
+++ View/Pages/RegistrationPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task6._1.Model;
+
+namespace Task6._1.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class RegistrationPage : Page
+    {
+        private ModelContext? _db = null;
+        private User _user = new User();
+        public RegistrationPage()
+        {
+            InitializeComponent();
+
+            _db = new ModelContext();
+        }
+
+
+    }
+}
